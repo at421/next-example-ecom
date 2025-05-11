6a0fa499254ce0cf27f0a6f440f0d62ee464c1fc
SwallowAI <alex@swallow.ai>
2025-05-11 23:12:06 +0000

    feat: migrate apis

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,14 +1,12 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 
-// fake login
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  const request = req.body;
-  const { email } = request;
-  const { password } = request;
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { email, password } = body;
 
   if (email === "johndoe@mail.com" && password === "ecommerce") {
-    res.status(200).json({ status: true });
+    return NextResponse.json({ status: true }, { status: 200 });
   } else {
-    res.status(401).json({ status: false });
+    return NextResponse.json({ status: false }, { status: 401 });
   }
-};
+}
