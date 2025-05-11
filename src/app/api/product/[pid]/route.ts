a72ed76dc276cc1e7596d2f0ba3e4c10fbbc5857
SwallowAI <alex@swallow.ai>
2025-05-11 16:30:29 +0000

    feat: migrate apis

--- src/app/api/product/[pid]/route.ts
+++ src/app/api/product/[pid]/route.ts
@@ -1,13 +1,13 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
+import { NextResponse } from "next/server";
 // fake data
-import products from "../../../utils/data/products";
+import products from "@/utils/data/products";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  const {
-    query: { pid },
-  } = req;
+export async function GET(
+  request: Request,
+  { params }: { params: { pid: string } }
+) {
+  const { pid } = params;
 
   const product = products.find((x) => x.id === pid);
-  res.status(200).json(product);
-};
+  return NextResponse.json(product, { status: 200 });
+}
